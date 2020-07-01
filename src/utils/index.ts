f22e94ba77408da9b3e70caf99e5a7a4122eb085
daiyu <daily.dai@transn.com>
2020-07-01 18:14:44 +0800

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ const files = (require as any).context('./', true, /(\.js|\.ts|\.tsx)$/);
 let tools: any = {};
 
 files.keys().forEach((key: any) => {
-  if (key === './index.js') return;
+  if (key === './index.js' || !files(key).default) return;
   tools = Object.assign(tools, files(key).default);
 });
 
